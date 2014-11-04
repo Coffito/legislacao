04ce2bf692125339fb2f08f11c7bcb81e6e19a74
Vinicius Ramos <vinicius@coffito.org.br>
2014-11-04 14:16:56 -0200

    alterando enconding do arquivo config.xml para iso-8859-1

--- platforms/android/ant-build/Legislacao-debug-unaligned.apk.d
+++ platforms/android/ant-build/Legislacao-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-c:\desenv\legislacao\platforms\android\ant-build\Legislacao-debug-unaligned.apk : \
-c:\desenv\legislacao\platforms\android\ant-build\Legislacao.ap_ \
-c:\desenv\legislacao\platforms\android\ant-build\classes.dex \
+C:\desenv\legislacao\platforms\android\ant-build\Legislacao-debug-unaligned.apk : \
+C:\desenv\legislacao\platforms\android\ant-build\Legislacao.ap_ \
+C:\desenv\legislacao\platforms\android\ant-build\classes.dex \
 C:\desenv\legislacao\platforms\android\CordovaLib\ant-build\classes.jar \
